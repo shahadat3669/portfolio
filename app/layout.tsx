195610ee51e27ec3fd7725559396c37a6650573d
Shahadat Hossain <shahadat3669@gmail.com>
2023-12-11 23:25:57 +0600

    :recycle: refactor(lint): resolve lint issues based on eslint and prettier configuration
    Address linting issues identified by ESLint and Prettier in the codebase. This commit focuses on
    resolving discrepancies and adhering to the defined coding standards and formatting rules. ESLint is
    a powerful tool for identifying potential problems in JavaScript code, while Prettier ensures a
    consistent code style across the project. The changes made in this commit include adjustments to
    variable naming, indentation, and other code style aspects to align with the project's established
    conventions. By addressing these linting issues, we aim to enhance code readability,
    maintainability, and overall code quality.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Open_Sans } from 'next/font/google';
-import './globals.css';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 
