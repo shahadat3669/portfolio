78f223912db528e064e6e3cd82ef761c6ef62c31
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-16 20:46:49 +0600

    :sparkles: feat: integrate CounterSection in the home page
    This commit introduces the CounterSection component, seamlessly integrated into the home page of the
    application. The CounterSection leverages the Counter component to showcase dynamic counting
    functionality, providing users with an engaging and interactive experience. The addition of this
    section enhances the overall content and visual appeal of the home page, contributing to a more
    captivating user interface.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import CounterSection from '../components/counter-section/CounterSection';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Shahadat Hossain</h1>
+    <main className="relative flex min-h-screen w-full flex-col items-center justify-between">
+      <CounterSection />
     </main>
   );
 }
