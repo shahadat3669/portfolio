821c0f4a3fdedb9c5f1672fcf411ddd9d543f4c1
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-21 00:20:05 +0600

    :sparkles: feat: implement Skill Section
    This commit introduces the Skill Section to the project, providing a dedicated space to showcase
    various skills. The section is designed to highlight expertise and proficiencies in different areas.
    Users can now populate and customize the Skill Section, creating a comprehensive display of their
    skills within the application.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import ContactSection from '@/components/contact-section/ContactSection';
-
-import CounterSection from '../components/counter-section/CounterSection';
+import CounterSection from '@/components/counter-section/CounterSection';
+import SkillsSection from '@/components/skills-section/SkillsSection';
 
 export default function Home() {
   return (
     <main className="relative flex min-h-screen w-full flex-col justify-start">
       <CounterSection />
       <ContactSection />
+      <SkillsSection />
     </main>
   );
 }
