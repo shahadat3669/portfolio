91d0a5af1cd37591215592c78f083ea8db4c4bf2
Shahadat Hossain <shahadat3669@gmail.com>
2024-02-21 09:32:30 +0600

    :recycle: refactor: refector the ProgressBar component props for meaningful simplicity
    This commit refines the ProgressBar component props to provide a more meaningful and simplified
    interface. By streamlining the props, users can now more intuitively configure and customize the
    ProgressBar component, promoting a cleaner and more concise implementation.

--- components/skills-section/ProgressBar.tsx
+++ components/skills-section/ProgressBar.tsx
@@ -3,32 +3,36 @@ import React from 'react';
 import { cn } from '@/libs/utils';
 
 type ProgressBarProps = {
-  bgColorClass?: string;
-  fgColorClass?: string;
+  containerClasses?: string;
+  bgClasses?: string;
+  progressClasses?: string;
   progress?: number;
-  heightClass?: string;
   text?: string;
 };
 
 const ProgressBar = ({
-  bgColorClass = 'bg-white/30',
+  bgClasses = 'bg-white/30 h-2',
   progress = 80,
-  heightClass = 'h-2',
-  fgColorClass = 'bg-white',
+  progressClasses = 'bg-white',
   text,
+  containerClasses,
 }: ProgressBarProps) => {
   return (
-    <div className="relative font-josefin-sense text-xl font-bold leading-[1.7] tracking-[-.2px] text-white">
+    <div
+      className={cn(
+        'relative font-josefin-sense text-xl font-bold leading-[1.7] tracking-[-.2px] text-white',
+        containerClasses
+      )}>
       {text && (
         <div className="mb-2 flex items-center justify-between text-lg">
           <div>{text}</div>
           <div>{progress}%</div>
         </div>
       )}
-      <div className={cn('flex overflow-hidden rounded text-xs', bgColorClass, heightClass)}>
+      <div className={cn('flex overflow-hidden rounded text-xs', bgClasses)}>
         <div
           style={{ width: `${progress}%` }}
-          className={cn('duration-[2s] delay-500 transition-all ease-out', fgColorClass)}
+          className={cn('duration-[2s] delay-500 transition-all ease-out', progressClasses)}
         />
       </div>
     </div>
